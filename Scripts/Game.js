25aa7f1ad16479f5c64cc560064bb4f1042126fc
Ben Carroll <ben.carroll@student.tsc.nsw.edu.au>
2016-03-03 14:06:19 +1100

    - Original Commit

---
+++ Scripts/Game.js
@@ -0,0 +1,151 @@
+
+//Function to set height of game pieces. Written by Ben Carroll.
+function setZoneHeight(){
+  for(var e=document.querySelectorAll(".colourzone"),n=0;n<e.length;n++){
+    e[n].style.height=window.innerHeight/4+"px"
+  }
+}
+
+// Sleep function, allowing us to 'pasue time' in js
+function sleep(e){for(var t=(new Date).getTime(),n=0;1e7>n&&!((new Date).getTime()-t>e);n++);}
+
+// Hex to RGB Function
+// function hexToRgb(a){var r=/^#?([a-f\d])([a-f\d])([a-f\d])$/i;a=a.replace(r,function(a,r,e,n){return r+r+e+e+n+n});var e=/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(a);return e?{r:parseInt(e[1],16),g:parseInt(e[2],16),b:parseInt(e[3],16)}:null}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Define game vars
+var userScore = 0;
+var losses = -1;
+var correctZone = "";
+var gameStarted = false;
+var currentColour = "";
+var currentZones = "";
+var cc = "";
+var lastClicked = 0;
+var lossSpeed = 3000;
+var gameEnded = false;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Game code
+
+function keypressCheck(stuff) {
+  if (gameStarted && gameEnded === false) {
+    code = stuff.keyCode;
+  if (code === 49) {
+    colourClicked(1)
+  } else if (code === 50) {
+    colourClicked(2)
+  } else if (code === 51) {
+    colourClicked(3)
+  } else if (code === 52) {
+    colourClicked(4)
+  }
+  onClickCheck();
+}
+}
+
+function startGame() {
+    if (gameStarted === false) {
+        gameStarted = true;
+        setColours();
+        document.getElementById('footerL').innerHTML = "Current Colour";
+    }
+}
+
+function getRandomColour() {
+    for (var o = "0123456789ABCDEF".split(""), n = "#", r = 0; 6 > r; r++) n += o[Math.floor(16 * Math.random())];
+    return n
+}
+
+function checkGameOver() {
+    if (losses >= 5) {
+        gameElements = document.querySelectorAll(".gameElements")
+        for (var i = 0; i < gameElements.length; i++) {
+            gameElements[i].style.display = 'none';
+        }
+        document.getElementById('loseTitle').style.display = "block";
+        document.getElementById('finalScore').innerHTML = userScore;
+        gameEnded = true;
+    }
+}
+
+function checkSpeed() {
+  if (gameStarted && gameEnded === false) {
+    if (userScore === 5) {
+      lossSpeed = 2500;
+    } else if (userScore === 10) {
+      lossSpeed = 2000;
+    } else if (userScore === 15) {
+      lossSpeed = 1500;
+    } else if (userScore === 20) {
+      lossSpeed = 1000;
+    } else if (userScore === 30) {
+      lossSpeed = 500;
+    }
+    document.getElementById('speed').innerHTML = lossSpeed/1000;
+  }
+}
+
+function setColours() {
+    randomArrayNumber = Math.floor(Math.random() * 4);
+    currentZones = [getRandomColour(), getRandomColour(), getRandomColour(), getRandomColour()]
+    currentColour = currentZones[randomArrayNumber];
+    currentZones2 = [currentZones[0],currentZones[1],currentZones[2],currentZones[3]]
+    document.getElementById('middle').style.backgroundColor = currentColour;
+    document.getElementById('colour1').style.backgroundColor = currentZones2[0];
+    document.getElementById('colour2').style.backgroundColor = currentZones2[1];
+    document.getElementById('colour3').style.backgroundColor = currentZones2[2];
+    document.getElementById('colour4').style.backgroundColor = currentZones2[3];
+}
+
+function onClickCheck() {
+  if (gameStarted && gameEnded === false) {
+    var timeNow = (new Date()).getTime();
+    if (timeNow > (lastClicked + lossSpeed)) {
+      losses = Number(losses + 1);
+      if (losses >= 5) {
+        document.getElementById('loseTitle2').innerHTML = "Too Slow!"
+      }
+      document.getElementById('losses').innerHTML = losses;
+      checkGameOver();
+    }
+    lastClicked = timeNow;
+  }
+}
+function setHighScore() {
+  if (userScore > Number(localStorage.getItem("highScore"))) {
+    localStorage.setItem("highScore", userScore);
+  } else {
+    localStorage.setItem("lastScore", userScore);
+  }
+}
+function colourClicked(zone) {
+    if (gameStarted && gameEnded === false) {
+        cc = document.getElementById('middle').style.backgroundColor;
+        if (document.getElementById('colour'+zone).style.backgroundColor == cc) {
+            userScore = Number(userScore + 1);
+            document.getElementById('score').innerHTML = userScore;
+            setColours();
+            checkSpeed();
+        }
+        // else if (zone == 5) {
+            // losses = Number(losses + 1);
+            // document.getElementById('losses').innerHTML = losses;
+            // checkGameOver();
+            // setColours();
+        // }
+        else {
+            losses = Number(losses + 1);
+            document.getElementById('losses').innerHTML = losses;
+            checkGameOver();
+            checkSpeed();
+            setColours();
+        }
+    } else {
+      startGame()
+    }
+}
---
+++ Scripts/Home.js
@@ -0,0 +1,64 @@
+// Minified version of colour changing background from CodeZag. (http://bit.ly/ccbackground)
+$(document).ready(function()
+{
+  function getRandomColor() {
+      for (var o = "0123456789ABCDEF".split(""), n = "#", r = 0; 6 > r; r++) n += o[Math.floor(16 * Math.random())];
+      return n
+  }
+
+  function change_random_bgcolor(o) {
+      setInterval(function() {
+          $("body").animate({
+              backgroundColor: getRandomColor()
+          }, 1000)
+      }, o)
+  }
+  change_random_bgcolor(200);
+});
+// End of colour changing background
+
+function checkBrowser() {
+  var canUPlay = ""
+  if (screen.width > 1400 && screen.height > 650) {
+    canUPlay = "\n\nTry playing in full-screen mode and reload the page.\n\nYour full-screen size is large enough to play this game."
+  } else {
+    canUPlay = "\n\nYour full-screen size is not large enough to play this game.\n\n You may experience some glitches while playing."
+  }
+  if (window.innerWidth > 1350 && window.innerHeight > 650) {
+    //Insert cool actions!
+      } else {
+        alert('Your current screen size is too small for this game!'+ canUPlay);
+      }
+}
+
+// Javascript sleep function, allowing us to 'pasue time' in js
+function sleep(e){for(var t=(new Date).getTime(),n=0;1e7>n&&!((new Date).getTime()-t>e);n++);}
+
+
+
+// A special title changing code that makes the title look like it is scrolling. Made by Ben Carroll.
+
+// Currently not being used as because it uses the sleep function to delay
+// in between each change, it also pauses the changing background
+var titlez=function(){sleep(200),document.title="Colour Craze",sleep(200),document.title="olour Craze - C",sleep(200),document.title="lour Craze - Co",sleep(200),document.title="our Craze - Col",sleep(200),document.title="ur Craze - Colo",sleep(200),document.title="r Craze - Colou",sleep(200),document.title=" Craze - Colour",sleep(200),document.title="Craze - Colour ",sleep(200),document.title="raze - Colour C",sleep(200),document.title="aze - Colour Cr",sleep(200),document.title="ze - Colour Cra",sleep(200),document.title="e - Colour Craz",sleep(200),document.title=" - Colour Craze",sleep(200),document.title="- Colour Craze",sleep(200),document.title=" Colour Craze",sleep(200),document.title="Colour Craze"};
+
+function hideWelcome(e) {
+  //Start hiding elements
+  hide1 = document.getElementById('herotext');
+  hide2 = document.getElementById('tutLarge');
+  hide3 = document.getElementById('footerL');
+  hide1.style.display='none';
+  hide2.style.display='none';
+  hide3.style.display='none';
+
+  //Start showing elements
+  if (e="true") {
+    show1 = document.querySelectorAll(".tut");
+
+    for (var i = 0; i < show1.length; i++) {
+        show1[i].style.display = 'inline';
+    }
+  }
+
+
+}
